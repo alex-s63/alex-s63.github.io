61780678683098c978a8b0402d76f32ecc8283a6
alex-s63 <alexey6449@gmail.com>
2019-04-29 16:56:39 +0300

    Change index.html path

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	chainWebpack: config => {
+		config
+			.plugin("html")
+			.tap(args => {
+				if (process.env.DEMO) {
+					args[0].template = "index.html"
+				}
+				return args
+			})
+	}
+}
